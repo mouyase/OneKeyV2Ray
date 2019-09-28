fa2e9ef6b5b248a63c7ec154b68aaff91b71ef5a
某亚瑟 <mouyase@qq.com>
2019-09-28 17:49:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ check_system(){
         cat>/etc/yum.repos.d/nginx.repo<<EOF
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/mainline/centos/7/\$basearch/
+baseurl=http://nginx.org/packages/centos/7/\$basearch/
 gpgcheck=0
 enabled=1
 EOF
